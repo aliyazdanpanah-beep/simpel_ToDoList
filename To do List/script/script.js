c5f77480b5fa562fcc5636b1e701915439413b3b
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-02-11 13:56:43 +0330

    update js

--- To do List/script/script.js
+++ To do List/script/script.js
@@ -4,5 +4,12 @@ let DELB = document.getElementById("DELB");
 
 ADDB.onclick = () => {
   const INPV = Input.value;
-  console.log(INPV);
+  if (INPV == "") {
+    let ListItem = document.createElement("li");
+    let TaskList = document.querySelector("#tasks ul");
+    TaskList.appendChild(ListItem);
+    ListItem.textContent = INPV;
+  } else {
+    alert("something wrong");
+  }
 };
