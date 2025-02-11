cff1039410406ca5500034d0a0ad5081f8e29c4d
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-02-11 13:42:13 +0330

    Update script.js

---
+++ To do List/script/script.js
@@ -0,0 +1,8 @@
+let Input = document.getElementById("Input");
+let ADDB = document.getElementById("ADDB");
+let DELB = document.getElementById("DELB");
+
+ADDB.onclick = () => {
+  const INPV = Input.value;
+  console.log(INPV);
+};
